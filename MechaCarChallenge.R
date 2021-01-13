944c7641c784e3a6f3b1883dc3cc5ba9af9037cb
MikaylaKurland <66794254+MikaylaKurland@users.noreply.github.com>
2021-01-12 22:15:38 -0500

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -22,3 +22,10 @@ total_summary <- df_SuspensionCoils %>% summarize(mean = mean(PSI), median = med
 #2.4
 lot_summary <- df_SuspensionCoils %>% group_by(Manufacturing_Lot) %>% summarize(mean = mean(PSI), median = median(PSI), var = var(PSI), sd = sd(PSI), .groups = 'keep')
 
+#3.1
+t.test(log10(df_SuspensionCoils$PSI),mu=mean(log10(1500)))
+
+#3.2
+t.test(log10(df_SuspensionCoils$PSI),df_SuspensionCoils$Manufacturing_Lot=="Lot1",mu=mean(log10(1500)))
+t.test(log10(df_SuspensionCoils$PSI),df_SuspensionCoils$Manufacturing_Lot=="Lot2",mu=mean(log10(1500)))
+t.test(log10(df_SuspensionCoils$PSI),df_SuspensionCoils$Manufacturing_Lot=="Lot3",mu=mean(log10(1500)))
