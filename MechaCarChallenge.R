8a6f20e042cf2bcf57bba6fcf72ead9d98f6e800
MikaylaKurland <66794254+MikaylaKurland@users.noreply.github.com>
2021-01-12 21:47:46 -0500

    Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,14 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #1.6
 summary( lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = df_MechaCarMPG))
+
+#2.2
+df_SuspensionCoils <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#2.3 
+total_summary <- df_SuspensionCoils %>% summarize(mean = mean(PSI), median = median(PSI), var = var(PSI), sd = sd(PSI), .groups = 'keep')
+
+
+#2.4
+lot_summary <- df_SuspensionCoils %>% group_by(Manufacturing_Lot) %>% summarize(mean = mean(PSI), median = median(PSI), var = var(PSI), sd = sd(PSI), .groups = 'keep')
+
